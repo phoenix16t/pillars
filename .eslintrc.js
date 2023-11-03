bfc1fd5ca4808891ccb414b372610b986385b118
Eddie Jimenez <eddiejimenez@Eddies-MacBook-Pro.local>
2023-11-03 14:22:52 -0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  "root": true,
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    'plugin:prettier/recommended'
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": { "project": ["./tsconfig.json"] },
+  "plugins": [
+    "@typescript-eslint"
+  ],
+  "rules": {
+    "@typescript-eslint/strict-boolean-expressions": [
+      2,
+      {
+        "allowString": false,
+        "allowNumber": false
+      }
+    ]
+  },
+  "ignorePatterns": ["src/**/*.test.ts", "src/frontend/generated/*", ".eslintrc.js"],
+  "overrides": [
+    {
+      "files": ['*.ts', '*.tsx'], // Your TypeScript files extension
+
+      // As mentioned in the comments, you should extend TypeScript plugins here,
+      // instead of extending them outside the `overrides`.
+      // If you don't want to extend any rules, you don't need an `extends` attribute.
+      "extends": [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+
+      "parserOptions": {
+        "project": ['./tsconfig.json'], // Specify it only for TypeScript files
+      },
+    },
+  ],
+  // ...
+}
