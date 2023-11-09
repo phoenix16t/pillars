db31bfec5f86b84b799af15973dbfa3df050af42
Eddie Jimenez <eddiejimenez@Eddies-MacBook-Pro.local>
2023-11-08 23:02:09 -0800

    optimizing the throttle import

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import { useState, useCallback, useMemo } from "react";
-import { throttle } from "lodash";
+import throttle from "lodash/throttle";
 
 import { TiltBase, Cube } from "components";
 
